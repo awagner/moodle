2d9ac5443e6231be968c004266e7c4c6dab81618
awagner <awagner@gmx.de>
2025-10-22 08:31:42 +0200

    MDL-86968 core_course: Set lock for move_section_to

--- public/course/lib.php
+++ public/course/lib.php
@@ -1085,7 +1085,7 @@ function course_module_calendar_event_update_process($instance, $cm) {
  */
 function move_section_to($course, $section, $destination, $ignorenumsections = false) {
 /// Moves a whole course section up and down within the course
-    global $USER, $DB;
+    global $DB;
 
     if (!$destination && $destination != 0) {
         return true;
@@ -1098,44 +1098,73 @@ function move_section_to($course, $section, $destination, $ignorenumsections = f
         return false;
     }
 
-    // Get all sections for this course and re-order them (2 of them should now share the same section number)
-    if (!$sections = $DB->get_records_menu('course_sections', array('course' => $course->id),
-            'section ASC, id ASC', 'id, section')) {
+    // We can't move section position 0.
+    if ($section < 1) {
         return false;
     }
 
-    $movedsections = reorder_sections($sections, $section, $destination);
+    // Get an instance of the currently configured lock_factory.
+    $lockfactory = \core\lock\lock_config::get_lock_factory('core_section_moveto');
 
-    // Update all sections. Do this in 2 steps to avoid breaking database
-    // uniqueness constraint
-    $transaction = $DB->start_delegated_transaction();
-    foreach ($movedsections as $id => $position) {
-        if ((int) $sections[$id] !== $position) {
-            $DB->set_field('course_sections', 'section', -$position, ['id' => $id]);
-            // Invalidate the section cache by given section id.
-            course_modinfo::purge_course_section_cache_by_id($course->id, $id);
+    // Request a lock for the resource (wait if necessary).
+    // Prevents parallel execution of the following lines by multiple processes,
+    // especially by core_course\task\course_delete_modules tasks.
+    // If a core_course\task\course_delete_modules task does not obtain the lock, it must be retried later.
+    if (!$lock = $lockfactory->get_lock('courseid_' . $course->id, 60)) {
+        throw new \moodle_exception('locktimeout', '', '', null, 'core_section_moveto: courseid_' . $course->id);
+    }
+
+    try {
+        // Get all sections for this course and re-order them (2 of them should now share the same section number).
+        $sections = $DB->get_records_menu(
+            'course_sections',
+            ['course' => $course->id],
+            'section ASC, id ASC',
+            'id, section'
+        );
+        if (!$sections) {
+            $lock->release();
+            return false;
         }
-    }
-    foreach ($movedsections as $id => $position) {
-        if ((int) $sections[$id] !== $position) {
-            $DB->set_field('course_sections', 'section', $position, ['id' => $id]);
-            // Invalidate the section cache by given section id.
-            course_modinfo::purge_course_section_cache_by_id($course->id, $id);
+
+        // Reorder sections may return false here, if the origin $section (number not id)
+        // we want to move to, is not found. In this case the section object to move is unknown,
+        // so we have to do an early exit here.
+        $movedsections = reorder_sections($sections, $section, $destination);
+        if (!$movedsections) {
+            $lock->release();
+            return false;
         }
-    }
 
-    // If we move the highlighted section itself, then just highlight the destination.
-    // Adjust the higlighted section location if we move something over it either direction.
-    if ($section == $course->marker) {
-        course_set_marker($course->id, $destination);
-    } else if ($section > $course->marker && $course->marker >= $destination) {
-        course_set_marker($course->id, $course->marker+1);
-    } else if ($section < $course->marker && $course->marker <= $destination) {
-        course_set_marker($course->id, $course->marker-1);
-    }
+        // Update all sections. Do this in 2 steps to avoid breaking database
+        // uniqueness constraint.
+        $transaction = $DB->start_delegated_transaction();
+        foreach ($movedsections as $id => $position) {
+            if ((int) $sections[$id] !== $position) {
+                $DB->set_field('course_sections', 'section', -$position, ['id' => $id]);
+            }
+        }
+        foreach ($movedsections as $id => $position) {
+            if ((int) $sections[$id] !== $position) {
+                $DB->set_field('course_sections', 'section', $position, ['id' => $id]);
+            }
+        }
 
-    $transaction->allow_commit();
-    rebuild_course_cache($course->id, true, true);
+        // If we move the highlighted section itself, then just highlight the destination.
+        // Adjust the highlighted section location if we move something over it either direction.
+        if ($section == $course->marker) {
+            course_set_marker($course->id, $destination);
+        } else if ($section > $course->marker && $course->marker >= $destination) {
+            course_set_marker($course->id, $course->marker + 1);
+        } else if ($section < $course->marker && $course->marker <= $destination) {
+            course_set_marker($course->id, $course->marker - 1);
+        }
+
+        $transaction->allow_commit();
+    } finally {
+        $lock->release();
+        rebuild_course_cache($course->id, true);
+    }
     return true;
 }
 
